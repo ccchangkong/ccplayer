a963e3c8220190e06cf5844483dfaffda2ff4ebc
ccchangkong <chlo0o@qq.com>
2016-12-19 16:48:46 +0800

    认真做啦！

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+// import store from './store'
+// import Axios from 'axios'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+Vue.use(MuseUI)
+  // Vue.prototype.$http = Axios
+  /* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
