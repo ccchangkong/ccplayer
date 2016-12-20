dd6482a01fe5f94ff53137f3b26703f1b6812d14
ccchangkong <chlo0o@qq.com>
2016-12-20 18:01:16 +0800

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,12 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    "quotes": 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,78 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <audio :src="songUrl" controls></audio>
+    <input type="text">
+    <p>{{songUrl}}</p>
+    <button>搜索</button>
   </div>
 </template>
-
 <script>
-import Hello from './components/Hello'
-
+// import Hello from './components/Hello'
 export default {
   name: 'app',
-  components: {
-    Hello
+  data () {
+    return {
+      audio: {
+        songUrl: '',
+        songId: '105552428',
+        imgUrl: 'http://m.kugou.com/v3/static/images/index/logo_kugou.png',
+        title: '',
+        singer: '',
+        currentLength: 0,
+        songLength: 0,
+        currentFlag: false
+      },
+      searchList: {
+        "code": 0,
+        "data": {
+          "keyword": "",
+          "priority": 0,
+          "qc": [],
+          "semantic": {
+            "curnum": 0,
+            "curpage": 1,
+            "list": [],
+            "totalnum": 0
+          },
+          "song": {
+            "curnum": 4,
+            "curpage": 1,
+            "list": [{
+              "albumName_hilight": "TVアニメ『この素晴らしい世界に祝福を! 』エンディング&#12539;テーマ「ちいさな冒険者」",
+              "chinesesinger": 0,
+              "docid": "12880189318695568719",
+              "f": "105552428|ちいさな冒険者|165909|雨宮天;高橋李依;茅野愛衣|1279297|TVアニメ『この素晴らしい世界に祝福を! 』エンディング&amp;#12539;テーマ「ちいさな冒険者」|1715890|199|7|1|1|7958257|3183491|320000|0|0|0|4134031|4553627|0|000xQKCb2bnAxr|004gbK7Y0nL8I8|003TtLh82YGCgK|0|8013"
+            }, {
+              "albumName_hilight": "TVアニメ『この素晴らしい世界に祝福を！』オリジナル&#12539;サウンドトラック",
+              "chinesesinger": 0,
+              "docid": "2481106511047243558",
+              "f": "105759298|ちいさな冒険者|165909|雨宮天;高橋李依;茅野愛衣|1307127|TVアニメ『この素晴らしい世界に祝福を！』オリジナル&amp;#12539;サウンドトラック|807102|93|7|1|1|3743150|1497455|750000|0|9092189|9163229|1965567|2084523|0|001IOPqp362aS1|004gbK7Y0nL8I8|000P4VxA1eEOmi|0|8013"
+            }],
+            "totalnum": 8
+          },
+          "totaltime": 2.000000000000000e-05,
+          "zhida": {
+            "chinesesinger": 0,
+            "type": 0
+          }
+        },
+        "message": "",
+        "notice": "",
+        "subcode": 0,
+        "time": 1482224695,
+        "tips": ""
+      }
+    }
+  },
+  computed: {
+    songUrl () { return `http://ws.stream.qqmusic.qq.com/${this.audio.songId}.m4a?fromtag=46` }
   }
+  // components: {
+  //   Hello
+  // }
 }
 </script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
