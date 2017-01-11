545d76821200fd926fe401ad88a4074bf91b7341
ccchangkong <chlo0o@qq.com>
2017-01-11 18:00:45 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!--<img src="./assets/logo.png">-->
+    <p @click="c2c('105552428|ちいさな冒険者')">2333</p>
     <audio :src="audio.songUrl" controls></audio>
     <input type="text" v-model="audio.keyWord">
     <p>{{songUrl}}</p>
     <button @click='ajax'>搜索</button>
     <ul>
-      <li v-for="i in cc">
-        {{ es(i.f) }}
+      <li v-for="(i,n) in cc">
+        <p @click="ccs(n)">{{ i }}</p>
       </li>
     </ul>
   </div>
@@ -28,7 +29,7 @@ export default {
         currentLength: 0,
         songLength: 0,
         currentFlag: false,
-        keyWord: ''
+        keyWord: '小小冒险者'
       },
       searchList: {
         "code": 0,
@@ -61,11 +62,12 @@ export default {
         "time": 1482224695,
         "tips": ""
       },
-      cc: ''
+      cc: []
     }
   },
   computed: {
-    es (f) { return f }
+    // es (f) { return `http://ws.stream.qqmusic.qq.com/${f}.m4a?fromtag=46` }
+    es () { return this.audio.keyWord + '!' }
   },
   methods: {
     ajax () {
@@ -75,20 +77,23 @@ export default {
       // let name = this.audio.keyWord
       let urlString = `/fcgi-bin/music_search_new_platform?t=0&n=${num}&aggr=1&cr=1&loginUin=0&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=jqminiframe.json&needNewCode=0&p=1&catZhida=0&remoteplace=sizer.newclient.next_song&w=${this.audio.keyWord}`
       $.get(urlString, function (data) {
-        console.log(JSON.parse(data))
         data = JSON.parse(data)
-        self.cc = data['data']['song']['list']
+        // self.cc = data['data']['song']['list']
         let es = data['data']['song']['list'][0]['f'].split('|')
-        // console.log(es[0] + ' ' + es[1])
         self.audio.songUrl = `http://ws.stream.qqmusic.qq.com/${es[0]}.m4a?fromtag=46`
-        // data['data']['song']['list'].forEach(
-        //   e => {
-        //     let es = e['f'].split('|')
-        //     self.audio.songUrl = `http://ws.stream.qqmusic.qq.com/${es[0]}.m4a?fromtag=46`
-        //     console.log(es[0] + ' ' + es[1])
-        //   }
-        // )
+        data['data']['song']['list'].forEach(
+          e => {
+            let es = e['f'].split('|')
+            self.cc.push(es[0])
+          }
+        )
       })
+    },
+    c2c: f => {
+      window.alert(f.split('|')[0])
+    },
+    ccs (f) {
+      this.audio.songUrl = `http://ws.stream.qqmusic.qq.com/${this.cc[f]}.m4a?fromtag=46`
     }
   }
   // components: {
