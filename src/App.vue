cc7f81836b05eb8c4ffda47cd21437c0c89568a9
ccchangkong <chlo0o@qq.com>
2017-02-05 22:24:45 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <p @click="c2c('105552428|ちいさな冒険者')">2333</p>
-    <audio :src="audio.songUrl" controls></audio>
+     
+        <img :src="audio.imgUrl" alt="">
+    <audio ref="player" :src="audio.songUrl" controls></audio>
     <input type="text" v-model="audio.keyWord">
     <p>{{songUrl}}</p>
     <button @click='ajax'>搜索</button>
@@ -31,44 +31,19 @@ export default {
         currentFlag: false,
         keyWord: '小小冒险者'
       },
-      searchList: {
-        "code": 0,
-        "data": {
-          "song": {
-            "curnum": 4,
-            "curpage": 1,
-            "list": [{
-              "albumName_hilight": "TVアニメ『この素晴らしい世界に祝福を! 』エンディング&#12539;テーマ「ちいさな冒険者」",
-              "chinesesinger": 0,
-              "docid": "12880189318695568719",
-              "f": "105552428|ちいさな冒険者|165909|雨宮天;高橋李依;茅野愛衣|1279297|TVアニメ『この素晴らしい世界に祝福を! 』エンディング&amp;#12539;テーマ「ちいさな冒険者」|1715890|199|7|1|1|7958257|3183491|320000|0|0|0|4134031|4553627|0|000xQKCb2bnAxr|004gbK7Y0nL8I8|003TtLh82YGCgK|0|8013"
-            }, {
-              "albumName_hilight": "TVアニメ『この素晴らしい世界に祝福を！』オリジナル&#12539;サウンドトラック",
-              "chinesesinger": 0,
-              "docid": "2481106511047243558",
-              "f": "105759298|ちいさな冒険者|165909|雨宮天;高橋李依;茅野愛衣|1307127|TVアニメ『この素晴らしい世界に祝福を！』オリジナル&amp;#12539;サウンドトラック|807102|93|7|1|1|3743150|1497455|750000|0|9092189|9163229|1965567|2084523|0|001IOPqp362aS1|004gbK7Y0nL8I8|000P4VxA1eEOmi|0|8013"
-            }],
-            "totalnum": 8
-          },
-          "totaltime": 2.000000000000000e-05,
-          "zhida": {
-            "chinesesinger": 0,
-            "type": 0
-          }
-        },
-        "message": "",
-        "notice": "",
-        "subcode": 0,
-        "time": 1482224695,
-        "tips": ""
-      },
-      cc: []
+      cc: [],
+      img: '',
+      imgW: []
     }
   },
-  computed: {
-    // es (f) { return `http://ws.stream.qqmusic.qq.com/${f}.m4a?fromtag=46` }
-    es () { return this.audio.keyWord + '!!' }
-  },
+  // beforeMount () {
+  //   for (let i = 0; i < 100; i++) {
+  //     this.imgW[i] = `http://imgcache.qq.com/music/photo/album_${i * 10}/97/${i * 10}_albumpic_1279297_0.jpg`
+  //   }
+  // },
+  // computed: {
+  //   es () { return `http://imgcache.qq.com/music/photo/album_${this.img}/97/${this.img}_albumpic_1279297_0.jpg` }
+  // },
   methods: {
     ajax () {
       let self = this
@@ -84,16 +59,21 @@ export default {
         data['data']['song']['list'].forEach(
           e => {
             let es = e['f'].split('|')
-            self.cc.push(es[0])
+            console.log(es)
+            self.cc.push({id: es[0], pic: es[4]})
+            // 尺寸只有300,500
           }
         )
       })
     },
-    c2c: f => {
-      window.alert(f.split('|')[0])
-    },
     ccs (f) {
-      this.audio.songUrl = `http://ws.stream.qqmusic.qq.com/${this.cc[f]}.m4a?fromtag=46`
+      this.audio.songUrl = `http://ws.stream.qqmusic.qq.com/${this.cc[f].id}.m4a?fromtag=46`
+      let width = 300
+      this.audio.imgUrl = `http://imgcache.qq.com/music/photo/album_${width}/${this.cc[f].pic % 100}/${width}_albumpic_${this.cc[f].pic}_0.jpg`
+      this.$nextTick(function () {
+        // window.alert(this.$refs.player.src)
+        this.$refs.player.play()
+      })
     }
   }
   // components: {
